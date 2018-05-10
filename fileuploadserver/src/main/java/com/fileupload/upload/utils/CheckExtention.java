d2762b99cd1ddf23bbfc0b03d1b392c3cec23046
sjaqjwor <lsklsk45@naver.com>
2018-05-10 23:04:48 +0900

    Modify 'CheckExtention' class
    add check file nulll

--- fileuploadserver/src/main/java/com/fileupload/upload/utils/CheckExtention.java
+++ fileuploadserver/src/main/java/com/fileupload/upload/utils/CheckExtention.java
@@ -1,19 +1,23 @@
 package com.fileupload.upload.utils;
 
-import com.fileupload.upload.exceptions.FileExtenstionException;
+import com.fileupload.upload.exceptions.FileExtentionException;
 import org.apache.tika.Tika;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class CheckExtention {
-    public static boolean check(MultipartFile multipartFile) throws IOException{
+
+    public static void check(MultipartFile multipartFile) throws IOException{
+
+        Optional.ofNullable(multipartFile).
+                orElseThrow(FileExtentionException::new);
+
         Tika t = new Tika();
+
         String mimeType= t.detect(multipartFile.getInputStream());
-        if(mimeType.split("/")[1].equals("jpeg")){
-            return true;
-        }else{
-            throw new FileExtenstionException();
-        }
+        
+        if(!mimeType.split("/")[1].equals("jpeg")) throw new FileExtentionException();
     }
 }
