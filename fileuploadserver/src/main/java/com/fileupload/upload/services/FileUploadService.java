b7b8c6e78a69414cab1df8d50395ef17817f6c5e
sjaqjwor <lsklsk45@naver.com>
2018-05-10 23:05:51 +0900

    Modify 'FileUploadService' class
    modify file paht

--- fileuploadserver/src/main/java/com/fileupload/upload/services/FileUploadService.java
+++ fileuploadserver/src/main/java/com/fileupload/upload/services/FileUploadService.java
@@ -1,19 +1,14 @@
 package com.fileupload.upload.services;
 
-import com.fileupload.upload.exceptions.FileExtenstionException;
 import com.fileupload.upload.utils.CheckExtention;
-import org.apache.tika.Tika;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.stream.Collectors;
 
 @Service
 public class FileUploadService {
@@ -23,21 +18,13 @@ public class FileUploadService {
 
     public void fileUpload(MultipartFile[] multipartFile) throws IOException{
 
-        File file = new File(dir);
-        if(!file.exists()){
-            //상위 폴더 없으면 안만들어짐
-            //file.mkdir();
-            file.mkdirs();
-        }
 
         for(MultipartFile m : multipartFile){
-            if(!CheckExtention.check(m)){
-                throw new FileExtenstionException();
-            }
+            CheckExtention.check(m);
         }
         for(MultipartFile m : multipartFile){
                 byte[] bytes = m.getBytes();
-                String filePath = dir+"/"+m.getOriginalFilename();
+                String filePath = System.getProperty("user.home")+dir+"/"+m.getOriginalFilename();
                 Path path = Paths.get(filePath);
                 Files.write(path,bytes);
 
