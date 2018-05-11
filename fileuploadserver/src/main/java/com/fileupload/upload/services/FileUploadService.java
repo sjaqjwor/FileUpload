925949e896dc445df5c9afa4d45ba77266fd584a
sjaqjwor <lsklsk45@naver.com>
2018-05-11 13:57:54 +0900

    Modify FileUploadService class
    add logic image name change to hasg
    
    and directory make logic

--- fileuploadserver/src/main/java/com/fileupload/upload/services/FileUploadService.java
+++ fileuploadserver/src/main/java/com/fileupload/upload/services/FileUploadService.java
@@ -1,34 +1,50 @@
 package com.fileupload.upload.services;
 
 import com.fileupload.upload.utils.CheckExtention;
+import com.fileupload.upload.utils.ImageHash;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 
 @Service
 public class FileUploadService {
 
-    @Value("${dir}")
+    @Value("${image.dir}")
     private String dir;
 
-    public void fileUpload(MultipartFile[] multipartFile) throws IOException{
 
 
+    public void fileUpload(MultipartFile[] multipartFile,String id,String k) throws IOException, InvalidKeySpecException, NoSuchAlgorithmException {
+
+        String hash = new ImageHash().createHash(id);
+
+        File file = new File(dir+"/"+id+"/"+k);
+        if(!file.exists()){
+            file.mkdirs();
+        }
         for(MultipartFile m : multipartFile){
             CheckExtention.check(m);
         }
         for(MultipartFile m : multipartFile){
                 byte[] bytes = m.getBytes();
-                String filePath = System.getProperty("user.home")+dir+"/"+m.getOriginalFilename();
+                String extention = parseExtention(m.getOriginalFilename());
+                String filePath = dir+"/"+id+"/"+k+"/"+hash+extention;
                 Path path = Paths.get(filePath);
                 Files.write(path,bytes);
 
         }
-
+    }
+    private String parseExtention(String file){
+        return file.substring(file.lastIndexOf("."));
     }
 }
